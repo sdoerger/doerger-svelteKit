bdcdad49fac80b012e62768e1ba97eade6bf0435
stefandoerger <doerger.stefan@gmail.com>
2021-09-15 22:12:54 +0200

    Add services section, #develop

--- src/lib/home/Intro-Section.svelte
+++ src/lib/home/Intro-Section.svelte
@@ -1,5 +1,20 @@
-<h2 class="text-sdGreen-400 lg:text-4xl">Intro</h2>
-
-<p>
-
-</p>
+<div class="mb-10">
+	<h2 class="text-sdGreen-400 lg:text-4xl mb-10">Intro</h2>
+	
+	<h3 class="mb-10">... Von Ihrer Idee</h3>
+	
+	<p class="mb-10">
+		Hi, ich bin Stefan, Software-Entwickler (Fullstack Web-Development) mit 4 Jahren Erfahrung. Mein
+		Beruf ist mein Hobby und umgekehrt: Ich liebe es, responsive Websites und Web-Apps zu
+		programmieren. Wichtig ist mir dabei, dass diese nicht wie aus Beton gemacht aussehen, mehr
+		können, als nur gut aussehen und ihre Besucher erreichen.
+	</p>
+	
+	<h3 class="mb-10">... zu Ihrer Website</h3>
+	
+	<p class="mb-10">
+		Für wen mache ich das? Für alle, deren Vision von Templates zerhackt wird und die Funktionalität
+		brauchen, die ein Baukasten nicht bietet. Ich biete einen Rundum-Service, von der Plaung bis zum
+		Hosting; mein Fokus liegt hierbei auf der Programmierung, Gestaltung und Beratung.
+	</p>
+</div>
--- src/routes/home/index.svelte
+++ src/routes/home/index.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 import IntroSection from "$lib/home/Intro-Section.svelte";
+import ServiceSection from "$lib/home/Service-Section.svelte";
 </script>
 
 <IntroSection />
+<ServiceSection />
