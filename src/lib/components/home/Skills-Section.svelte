956a8742000bd59bb4783569ffb7a4de4218a48e
stefandoerger <doerger.stefan@gmail.com>
2021-09-17 23:39:39 +0200

    Add skills section, #develop

---
+++ src/lib/components/home/Skills-Section.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import ScrollSpy from '$lib/components/Elements/ScrollSpy.svelte';
+
+  const sectionTitle = 'Skills'
+</script>
+
+<ScrollSpy sectionTitle="{sectionTitle}" sectionSubtitle="Programmiersprachen">
+	<section class="index-section px-12 pt-36 lg:top-16 lg:left-0 lg:p-20 lg:pt-36 text-sdlightGrey-50 lg:text-2xl">
+		<div class="mb-10">
+			<p>Folgende</p>
+		</div>
+	</section>
+</ScrollSpy>
+
+<ScrollSpy sectionTitle="{sectionTitle}" sectionSubtitle="Frameworks/Libraries">
+	<section class="index-section px-12 pt-36 lg:top-16 lg:left-0 lg:p-20 lg:pt-36 text-sdlightGrey-50 lg:text-2xl">
+		<div class="mb-10">
+			<p>Folgende</p>
+		</div>
+	</section>
+</ScrollSpy>
--- src/routes/home/index.svelte
+++ src/routes/home/index.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import IntroSection from '$lib/components/home/Intro-Section.svelte';
 	import ServiceSection from '$lib/components/home/Service-Section.svelte';
+	import SkillsSection from '$lib/components/home/Skills-Section.svelte';
 </script>
 
 <IntroSection />
 <ServiceSection />
+<SkillsSection />
