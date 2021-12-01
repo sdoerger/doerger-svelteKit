77f9ad766839ab8b426221a397fd1e7e3dc46598
stefandoerger <doerger.stefan@gmail.com>
2021-12-01 23:42:31 +0100

    Adjust space to work, #develop

--- src/lib/components/Home/SkillsSection.svelte
+++ src/lib/components/Home/SkillsSection.svelte
@@ -8,39 +8,41 @@
 	const sectionTitle = 'Skills';
 </script>
 
-<ScrollSpy
-	{sectionTitle}
-	sectionSubtitle="Programmiersprachen"
-	dynamicSVGs="SkillLanguagesSVG"
->
-	<section id="skill-section" class="index-section">
-		<div class="mb-6">
-			<h2
-				class="pb-4 tracking-tighter font-bold uppercase leading-none md:mx-auto font-josefinSans text-3xl mt-20 break-words"
-			>
-				Programmiersprachen
-			</h2>
-			<p class="mb-16">Hier eine Auswahl der Programmiersprachen die ich täglich nutze.</p>
-			<SkillsLanguages />
-		</div>
-	</section>
-</ScrollSpy>
-
-<ScrollSpy
-	{sectionTitle}
-	sectionSubtitle="Frameworks/Libraries"
-	dynamicSVGs="SkillFrameworksSVG"
->
-	<section class="index-section">
-		<div class="mt-28 mb-6">
-			<h2
-				class="pb-4 tracking-tighter font-bold uppercase leading-none md:mx-auto font-josefinSans text-3xl mt-20"
-			>
-				Frameworks
-			</h2>
-			<p class="mb-16">Hier eine Auswahl der Frameworks die ich täglich nutze.</p>
-
-			<SkillsFrameworks />
-		</div>
-	</section>
-</ScrollSpy>
+<div class="mb-48">
+	<ScrollSpy
+		{sectionTitle}
+		sectionSubtitle="Programmiersprachen"
+		dynamicSVGs="SkillLanguagesSVG"
+	>
+		<section id="skill-section" class="index-section">
+			<div class="mb-6">
+				<h2
+					class="pb-4 tracking-tighter font-bold uppercase leading-none md:mx-auto font-josefinSans text-3xl mt-20 break-words"
+				>
+					Programmiersprachen
+				</h2>
+				<p class="mb-16">Hier eine Auswahl der Programmiersprachen die ich täglich nutze.</p>
+				<SkillsLanguages />
+			</div>
+		</section>
+	</ScrollSpy>
+	
+	<ScrollSpy
+		{sectionTitle}
+		sectionSubtitle="Frameworks/Libraries"
+		dynamicSVGs="SkillFrameworksSVG"
+	>
+		<section class="index-section">
+			<div class="mt-28 mb-6">
+				<h2
+					class="pb-4 tracking-tighter font-bold uppercase leading-none md:mx-auto font-josefinSans text-3xl mt-20"
+				>
+					Frameworks
+				</h2>
+				<p class="mb-16">Hier eine Auswahl der Frameworks die ich täglich nutze.</p>
+	
+				<SkillsFrameworks />
+			</div>
+		</section>
+	</ScrollSpy>
+</div>
